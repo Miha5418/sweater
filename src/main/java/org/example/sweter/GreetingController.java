69a4f87ddd64c5fd9094a344f30fa993a9e86104
Karpeykin Mihail <karpejkin2016@yandex.ru>
2021-01-11 23:54:56 +0700

    Урок 2 Подключил базу данных, Добавил возможность добавлять в неё записи, и филтровать их

--- src/main/java/org/example/sweter/GreetingController.java
+++ src/main/java/org/example/sweter/GreetingController.java
@@ -1,14 +1,20 @@
 package org.example.sweter;
 
+import org.example.sweter.doamin.Message;
+import org.example.sweter.repos.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
 @Controller
 public class GreetingController {
+    @Autowired
+    private MessageRepo messageRepo;
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World")
@@ -19,7 +25,34 @@ public class GreetingController {
 
     @GetMapping
     public String main(Map<String, Object> model) {
-        model.put("some", "Добро пожаловать");
+        Iterable<Message> messages = messageRepo.findAll();
+
+        model.put("messages", messages);
+
+        return "main";
+    }
+
+    @PostMapping
+    public String add(@RequestParam String text, @RequestParam String tag,
+                                  Map<String, Object> model){
+        Message message = new Message(text, tag);
+        messageRepo.save(message);
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages", messages);
+        return "main";
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter,
+                         Map<String, Object> model){
+        Iterable<Message> messages;
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        } else {
+            messages = messageRepo.findAll();
+        }
+        model.put("messages", messages);
+
         return "main";
     }
 
